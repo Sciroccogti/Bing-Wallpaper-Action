c520c66f48a983032459d6c7fcc087a2ee49de9d
张逸帆 <scirocco_gti@yeah.net>
2023-02-28 11:51:51 +0800

    fix workflow

--- replace_img.py
+++ replace_img.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     loc = sys.argv[1]
     data = requests.get(f"https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8&mkt={loc}").json()
     imageURL = data["images"][0]["url"]
-    image = requests.get(imageURL)
+    image = requests.get(baseURL + imageURL)
     with open("./data/zh-CN.jpg", "wb") as file:
         file.write(image.content)
